015a3217fdf3aa43730e806197e946c11c86ece2
Dave Hylands <dhylands@gmail.com>
2015-06-11 15:04:52 -0700

    Updated OSX colors to match what I use with linux

--- dot-startup.d/04-colors.sh
+++ dot-startup.d/04-colors.sh
@@ -37,7 +37,41 @@ NO_COLOR="\[\033[0m\]"
 
 if [ "$(uname)" == "Darwin" ]; then
     export CLICOLOR=1
-    export LSCOLORS=gxBxhxDxfxhxhxhxhxcxcx
+    # For LSCOLORS - these letters designate which color to use:
+    #   a     black
+    #   b     red
+    #   c     green
+    #   d     brown
+    #   e     blue
+    #   f     magenta
+    #   g     cyan
+    #   h     light grey
+    #   A     bold black, usually shows up as dark grey
+    #   B     bold red
+    #   C     bold green
+    #   D     bold brown, usually shows up as yellow
+    #   E     bold blue
+    #   F     bold magenta
+    #   G     bold cyan
+    #   H     bold light grey; looks like bright white
+    #   x     default foreground or background
+    #
+    # And this is the order:
+    #
+    #   1.   directory
+    #   2.   symbolic link
+    #   3.   socket
+    #   4.   pipe
+    #   5.   executable
+    #   6.   block special
+    #   7.   character special
+    #   8.   executable with setuid bit set
+    #   9.   executable with setgid bit set
+    #   10.  directory writable to others, with sticky bit
+    #   11.  directory writable to others, without sticky bit
+
+    #               1 2 3 4 5 6 7 8 9 1011
+    export LSCOLORS=GxBxhxDxCxhxhxHBhdhcGc
 else
     #
     # NOTE: dircolors is sensitive to the setting of the TERM environment variable
